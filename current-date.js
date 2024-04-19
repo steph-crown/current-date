2033567ab0f0769f4526ccc2feeaf1c72dd6563b
Steph Crown <emmanuelstephen024@gmail.com>
2024-04-19 10:55:49 +0100

    Add shadow DOM

--- current-date.js
+++ current-date.js
@@ -6,13 +6,30 @@ class CurrentDate extends HTMLElement {
   }
 
   connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "open" }); // Create Shadow DOM
+
     const options = {
       day: "numeric",
       month: "long",
       year: "numeric",
     };
 
-    this.textContent = this.currentDate.toLocaleDateString("en-US", options);
+    const formattedDate = this.currentDate.toLocaleDateString("en-US", options);
+
+    shadowRoot.innerHTML = `
+    <style>
+      p {
+        text-align: center;
+        font-weight: normal;
+        padding: 1em;
+        margin: 0 0 2em 0;
+        background-color: #eee;
+        border: 1px solid #666;
+        color: red;
+      }
+    </style>
+
+    <p>${formattedDate}</p>`;
   }
 }
 
